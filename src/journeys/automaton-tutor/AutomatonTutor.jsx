14ceabc677359a3b7ee92fc94fba3cc5beaf14cc
Awelani Murovhi <u18335412@tuks.co.za>
2022-08-03 00:54:41 +0200

    Added handler for long press and functionality for getting position of the press

--- src/journeys/automaton-tutor/AutomatonTutor.jsx
+++ src/journeys/automaton-tutor/AutomatonTutor.jsx
@@ -1,18 +1,58 @@
-import React, { useState } from 'react';
-import { RadialContextMenu } from '../../components/context-menus/RadialContextMenu';
+import React, { useState, useRef } from "react";
+import { RadialContextMenu } from "../../components/context-menus/RadialContextMenu";
+import { AiOutlinePlus, AiOutlineBgColors } from "react-icons/ai";
+import { IoResize } from "react-icons/io5";
+import useMouse from "@react-hook/mouse-position";
+import { useLongPress } from "use-long-press";
+
+const menuItems = [
+  {
+    label: "Add Automata",
+    icon: <AiOutlinePlus />,
+    action: () => console.log("Add Automata"),
+  },
+  {
+    label: "Change Background",
+    icon: <AiOutlineBgColors />,
+    action: () => console.log("Change Background"),
+  },
+  {
+    label: "Resize Automatons",
+    icon: <IoResize />,
+    action: () => console.log("Resize Automatons"),
+  },
+];
 
 export const AutomatonTutor = () => {
+  const containerRef = useRef(null);
+  const toggleButtonRef = useRef(null);
+  const mouse = useMouse(containerRef);
+
   const [contextMenuState, setContextMenuState] = useState({
-    isOpen: false,
     x: 0,
     y: 0,
-    items: [],
   });
 
+  const bind = useLongPress(() => {
+    toggleButtonRef.current.click();
+    setContextMenuState({
+      x: mouse.x - 25,
+      y: mouse.y,
+    });
+  });
   return (
-    <div>
-      <div>AutomatonTutor</div>
-      <RadialContextMenu {...contextMenuState} />
+    <div
+      {...bind()}
+      ref={containerRef}
+      className="relative w-full h-screen bg-slate-300 "
+    >
+      <RadialContextMenu
+        position={contextMenuState}
+        toggleButton={<button hidden ref={toggleButtonRef}></button>}
+        mouse={mouse}
+        items={menuItems}
+        {...contextMenuState}
+      />
     </div>
   );
 };
