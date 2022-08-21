1c122e54c4c69571697bfc4156cbd8e00334c517
Awelani Murovhi <u18335412@tuks.co.za>
2022-08-22 01:11:01 +0200

    updated context-menu

--- src/journeys/automaton-tutor/components/ContextMenu.jsx
+++ src/journeys/automaton-tutor/components/ContextMenu.jsx
@@ -1,14 +1,42 @@
 import React from "react";
 import useAutomatonTutorStore, { Context } from "../state/zustand";
-import { StateContext, CanvasContext } from "./context-menu/ContextItems";
+import {
+  StateContext,
+  CanvasContext,
+  TransitionContext,
+} from "./context-menu/ContextItems";
+import { AiOutlineMenu } from "react-icons/ai";
 
 const ContextMenu = () => {
   const { activeContexMenu } = useAutomatonTutorStore();
   return (
-    <div className="fixed flex justify-center w-full text-white">
-      <div className="flex px-4 py-2 mt-2 text-black rounded-md jusify-between bg-black/20 gap-x-8">
+    <div className="fixed flex items-center justify-center w-full pt-2 gap-x-2 h-fit">
+      <div className="flex px-4 h-full py-2  text-black rounded-md dark:text-white jusify-between bg-gray-300 [&>*]:transition-all gap-x-8 [&>*]:tooltip-bottom [&>*]:tooltip">
         {activeContexMenu === Context.State && <StateContext />}
         {activeContexMenu === Context.Canvas && <CanvasContext />}
+        {activeContexMenu === Context.Transition && <TransitionContext />}
+      </div>
+      <div className="flex items-center">
+        <div className="dropdown dropdown-end">
+          <AiOutlineMenu
+            tabIndex={0}
+            className="w-10 h-full px-2 py-2 text-black bg-gray-300 rounded-md "
+          />
+          <ul
+            tabIndex="0"
+            className="p-2 mt-4 text-sm font-semibold shadow-xl bg-slate-200 dropdown-content menu rounded-box w-52"
+          >
+            <li>
+              <a>Save game</a>
+            </li>
+            <li>
+              <a>Restart</a>
+            </li>
+            <li>
+              <a href="/">Exit game</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
--- src/journeys/automaton-tutor/components/context-menu/ContextItems.js
+++ src/journeys/automaton-tutor/components/context-menu/ContextItems.js
@@ -4,48 +4,96 @@ import { HiOutlineColorSwatch } from "react-icons/hi";
 import { CgRename } from "react-icons/cg";
 import { FiCheckCircle } from "react-icons/fi";
 import { BsArrowRight, BsArrowLeftRight } from "react-icons/bs";
+import useAutomatonTutorStore from "../../state/zustand.js";
+import uuid from "react-uuid";
 
-export const CanvasContext = () => (
-  <>
-    <button>
-      <AiOutlinePlus className="w-6 h-6 md:w-7 md:h-7" />
-    </button>
-    <button>
-      <HiOutlineColorSwatch className="w-6 h-6 md:w-7 md:h-7" />
-    </button>
-    <button>
-      <TbResize className="w-6 h-6 md:w-7 md:h-7" />
-    </button>
-  </>
-);
+export const CanvasContext = () => {
+  const { addState, graphData } = useAutomatonTutorStore();
+  const handleAddState = () => {
+    const stateId = uuid();
+    const newState = {
+      id: stateId,
+      name: `s${graphData.nodes.length + 1}`,
+      val: 5,
+    };
+    addState(newState);
+  };
 
-export const StateContext = () => (
-  <>
-    <button>
-      <BsArrowRight className="w-6 h-6 md:w-7 md:h-7" />
-    </button>
-    <button>
-      <FiCheckCircle className="w-6 h-6 md:w-7 md:h-7" />
-    </button>
-    <button>
-      <BsArrowLeftRight className="w-6 h-6 md:w-7 md:h-7" />
-    </button>
-    <button>
-      <CgRename className="w-6 h-6 md:w-7 md:h-7" />
-    </button>
-    <button>
-      <AiOutlineDelete className="w-6 h-6 md:w-7 md:h-7" />
-    </button>
-  </>
-);
+  return (
+    <>
+      <button data-tip="Add State" onClick={handleAddState}>
+        <AiOutlinePlus className="w-6 h-6 md:w-7 md:h-7" />
+      </button>
+      <button
+        data-tip="Toggle Background"
+        onClick={() => console.log("Toggle Background Color Clicked")}
+      >
+        <HiOutlineColorSwatch className="w-6 h-6 md:w-7 md:h-7" />
+      </button>
+      <button
+        data-tip="Resize States"
+        onClick={() => console.log("Resize states Clicked")}
+      >
+        <TbResize className="w-6 h-6 md:w-7 md:h-7" />
+      </button>
+    </>
+  );
+};
 
-export const TransitionContext = () => (
-  <>
-    <button>
-      <AiOutlineDelete className="w-6 h-6 md:w-7 md:h-7" />
-    </button>
-    <button>
-      <CgRename className="w-6 h-6 md:w-7 md:h-7" />
-    </button>
-  </>
-);
+export const StateContext = () => {
+  const { removeState, selectedEntity } = useAutomatonTutorStore();
+  return (
+    <>
+      <button
+        data-tip="Initial State"
+        onClick={() => console.log("Toggle Initial State Clicked")}
+      >
+        <BsArrowRight className="w-6 h-6 md:w-7 md:h-7" />
+      </button>
+      <button
+        data-tip="Final State"
+        onClick={() => console.log("Toggle Final State Clicked")}
+      >
+        <FiCheckCircle className="w-6 h-6 md:w-7 md:h-7" />
+      </button>
+      <button
+        data-tip="Add Transition"
+        onClick={() => console.log("Add Transition Clicked")}
+      >
+        <BsArrowLeftRight className="w-6 h-6 md:w-7 md:h-7" />
+      </button>
+      <button
+        data-tip="Remane"
+        onClick={() => console.log("Rename State Clicked")}
+      >
+        <CgRename className="w-6 h-6 md:w-7 md:h-7" />
+      </button>
+      <button
+        data-tip="Delete State"
+        onClick={() => removeState(selectedEntity)}
+      >
+        <AiOutlineDelete className="w-6 h-6 md:w-7 md:h-7" />
+      </button>
+    </>
+  );
+};
+
+export const TransitionContext = () => {
+  const { removeTransition, selectedEntity } = useAutomatonTutorStore();
+  return (
+    <>
+      <button
+        data-tip="Delete Transition"
+        onClick={() => removeTransition(selectedEntity)}
+      >
+        <AiOutlineDelete className="w-6 h-6 md:w-7 md:h-7" />
+      </button>
+      <button
+        data-tip="Rename Transition"
+        onClick={() => console.log("Rename Transition Clicked")}
+      >
+        <CgRename className="w-6 h-6 md:w-7 md:h-7" />
+      </button>
+    </>
+  );
+};
