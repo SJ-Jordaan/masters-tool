5a0aa3d9edbc90f3b34744f0701fbae8ffc3387d
Awelani Murovhi <u18335412@tuks.co.za>
2022-08-22 01:13:41 +0200

    state for graph and context-menu

--- src/journeys/automaton-tutor/state/zustand.js
+++ src/journeys/automaton-tutor/state/zustand.js
@@ -1,4 +1,5 @@
 import create from "zustand";
+import { persist } from "zustand/middleware";
 
 export const Context = {
   State: "State",
@@ -6,59 +7,79 @@ export const Context = {
   Transition: "Transition",
 };
 
-const useAutomatonTutorStore = create((set) => ({
-  graphData: {
-    nodes: [
-      {
-        id: "id1",
-        name: "name1",
-        val: 5,
-      },
-      {
-        id: "id2",
-        name: "name2",
-        val: 5,
-      },
-    ],
-    links: [
-      {
-        source: "id1",
-        target: "id2",
-      },
-    ],
-  },
-  activeContexMenu: Context.Canvas,
-  setActiveContexMenu: (context) => set({ activeContexMenu: context }),
-  addState: (newState) => (state) => ({
-    graphData: {
-      nodes: [...state.graphData.nodes, newState],
-      links: state.graphData.links,
-    },
-  }),
-  removeState: (stateIndex) => (state) => ({
+const useAutomatonTutorStore = create(
+  (set, get) => ({
     graphData: {
       nodes: [
-        ...state.graphData.nodes.filter((node) => node.id !== stateIndex),
+        {
+          id: "id1",
+          name: "name1",
+          val: 5,
+          isInitial: true,
+        },
+        {
+          id: "id2",
+          name: "name2",
+          val: 5,
+          isFinal: true,
+        },
+        {
+          id: "id3",
+          name: "name2",
+          val: 5,
+        },
       ],
-      links: state.graphData.links,
+      links: [
+        {
+          source: "id1",
+          target: "id3",
+        },
+      ],
     },
+    selectedEntity: null,
+    setSelectedEntity: (entity) =>
+      set((state) => ({ ...state, selectedEntity: entity })),
+    activeContexMenu: Context.Canvas,
+    setActiveContexMenu: (context) =>
+      set((state) => ({ ...state, activeContexMenu: context })),
+    addState: (newState) =>
+      set((state) => ({
+        graphData: {
+          nodes: [...state.graphData.nodes, newState],
+          links: state.graphData.links,
+        },
+      })),
+    removeState: (stateIndex) =>
+      set((state) => ({
+        graphData: {
+          nodes: [
+            ...state.graphData.nodes.filter((node) => node.id !== stateIndex),
+          ],
+          links: state.graphData.links,
+        },
+      })),
+    addTransition: (newTransition) =>
+      set((state) => ({
+        graphData: {
+          nodes: state.graphData.nodes,
+          links: [...state.graphData.links, newTransition],
+        },
+      })),
+    removeTransition: (transitionIndex) =>
+      set((state) => ({
+        graphData: {
+          nodes: state.graphData.nodes,
+          links: [
+            ...state.graphData.links.filter(
+              (link) => link.id !== transitionIndex
+            ),
+          ],
+        },
+      })),
   }),
-  addTransition: (newTransition) => (state) => ({
-    graphData: {
-      nodes: state.graphData.nodes,
-      links: [...state.graphData.links, newTransition],
-    },
-    removeTransition: (transitionIndex) => (state) => ({
-      graphData: {
-        nodes: state.graphData.nodes,
-        links: [
-          ...state.graphData.links.filter(
-            (link) => link.id !== transitionIndex
-          ),
-        ],
-      },
-    }),
-  }),
-}));
+  {
+    name: "automaton-tutor",
+  }
+);
 
 export default useAutomatonTutorStore;
