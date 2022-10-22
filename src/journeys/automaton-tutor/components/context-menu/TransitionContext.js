c511ff06915262a6d3f0f402a0222c899cea3c08
Awelani Murovhi <u18335412@tuks.co.za>
2022-10-22 18:26:33 +0200

    trigger for edit transition modal added

--- src/journeys/automaton-tutor/components/context-menu/TransitionContext.js
+++ src/journeys/automaton-tutor/components/context-menu/TransitionContext.js
@@ -1,22 +1,33 @@
 import { AiOutlineDelete } from "react-icons/ai";
 import { CgRename } from "react-icons/cg";
-import useAutomatonTutorStore from "../../state/useAutomatonTutorStore.js";
-
+import useAutomatonTutorStore, {
+  Modal,
+} from "../../state/useAutomatonTutorStore.js";
 export const TransitionContext = () => {
-  const { removeTransition, selectedEntity } = useAutomatonTutorStore();
+  const {
+    removeTransition,
+    selectedEntity,
+    setActiveModal,
+    setSelectedEntity,
+    setTargetState,
+  } = useAutomatonTutorStore();
   const handleRemoveTransition = () => {
     removeTransition(selectedEntity.index);
   };
+
+  const handleEditTransition = () => {
+    setTargetState(selectedEntity?.target);
+    setSelectedEntity(selectedEntity?.source);
+    setActiveModal(Modal?.EditTransition);
+  };
+
   return (
     <>
+      <button data-tip="Transition Values" onClick={handleEditTransition}>
+        <CgRename className="w-6 h-6 md:w-7 md:h-7" />
+      </button>
       <button data-tip="Delete Transition" onClick={handleRemoveTransition}>
         <AiOutlineDelete className="w-6 h-6 md:w-7 md:h-7" />
-      </button>
-      <button
-        data-tip="Transition Values"
-        onClick={() => console.log("Rename Transition Clicked")}
-      >
-        <CgRename className="w-6 h-6 md:w-7 md:h-7" />
       </button>
     </>
   );
